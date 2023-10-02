18ff8c81f38f00b8c3863478b8335d1fafcdda1f
Your Name <you@example.com>
2023-10-03 00:33:31 +0300

    PTLS-713 fix: hotfix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = parseInt(process.env.NODE_PORT || "3000", 10);
 const dev = process.env.BASE_ENV !== "prod";
 const app = next({ dev });
 const handle = app.getRequestHandler();
