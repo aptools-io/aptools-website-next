48b01dd4b4064319b38a3f618434f633e9919b4f
Your Name <you@example.com>
2023-10-04 02:13:03 +0300

    fix: logs on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ import { setHeaders } from "src/scripts/redux/slices/headersSlice";
 import { MainPage } from "src/components/pages";
 
 // API
-import { contractAddresses, contractTransactions, dexesVolumes, generalStats, projects, transactions } from "src/scripts/api/requests";
+import { contractAddresses, contractTransactions, dexesVolumes, generalStats, logger, projects, transactions } from "src/scripts/api/requests";
 
 // Scripts
 import categories from "src/scripts/consts/categories";
@@ -23,6 +23,7 @@ import filtrateProjects from "src/scripts/util/filtrateProjects";
 // Websocket
 import { aptosStats } from "src/scripts/websocket/connections";
 import { setPageTitle } from "src/scripts/redux/slices/pageTitleSlice";
+import { timeSpent } from "src/scripts/util/time";
 
 const Home = (data: IApiProps) => {
     const ws = useRef<WebSocket>(null);
@@ -51,68 +52,104 @@ const Home = (data: IApiProps) => {
 export default Home;
 
 export async function getServerSideProps(context) {
+    const start = Date.now();
+    const startTime = timeSpent(start);
     const projectsUnfiltered = (await projects.getData()) || [];
+    const projectsTime = timeSpent(start);
+
     const { req } = context;
 
     const blockchainStatistics = (await generalStats.getBlockchainStatisticsData()) || ({} as any);
+    const blockchainStatisticsTime = timeSpent(start);
+
     const dexesStatistics = (await generalStats.getDexesStatisticsData()) || ({} as any);
+    const dexesStatisticsTime = timeSpent(start);
+
     const tokenStatistics = (await generalStats.getTokenStatisticsData()) || ({} as any);
+    const tokenStatisticsTime = timeSpent(start);
+
+    const transactionsData = (await transactions.getData()) || [];
+    const transactionsDataTime = timeSpent(start);
+
+    const general_stats =
+        {
+            blockchain_info: {
+                ...blockchainStatistics,
+                low_high_price: tokenStatistics.low_high_price || {},
+                token_price_chart: tokenStatistics.token_price_chart || [],
+                trans_history: blockchainStatistics.transactions_history || [],
+                dex_trading_volume: dexesStatistics.dex_trading_volume || null,
+                total_value_locked: dexesStatistics.total_value_locked || null,
+                vol_24h: dexesStatistics.vol_24h || null
+            },
+
+            dex_tvl: dexesStatistics.dex_tvl_chart || [],
+            dex_volumes: dexesStatistics.dex_volumes_chart || [],
+            daily_unique_contract_addresses: dexesStatistics.dex_addresses_chart || [],
+            daily_contract_transactions: dexesStatistics.dex_transactions_chart || [],
+            markets: dexesStatistics.dex_markets || [],
+            token_statistics: tokenStatistics.token_statistics || [],
+            top_tokens_by_volume: tokenStatistics.top_tokens_by_volume || [],
+
+            top_statistics: blockchainStatistics.money_flow || {},
+            transactions_plot: blockchainStatistics.user_transactions_history || [],
+            addresses_plot: blockchainStatistics.addresses_plot || [],
+            daily_new_created_wallets: blockchainStatistics.daily_new_created_wallets || [],
+            active_unique_addresses: blockchainStatistics.active_unique_addresses || {}
+        } || {};
+
+    const contract_addresses = dexesStatistics.dex_users_activity?.length
+        ? dexesStatistics.dex_users_activity.map((item) => {
+              return {
+                  ...item,
+                  "24h": item?.["24h"]?.addresses,
+                  all_time: item?.["all_time"]?.addresses,
+                  week: item?.["week"]?.addresses
+              };
+          })
+        : [];
+
+    const contract_transactions = dexesStatistics.dex_users_activity?.length
+        ? dexesStatistics.dex_users_activity.map((item) => {
+              return {
+                  ...item,
+                  "24h": item?.["24h"]?.transactions,
+                  all_time: item?.["all_time"]?.transactions,
+                  week: item?.["week"]?.transactions
+              };
+          })
+        : [];
+
+    const projectsData = filtrateProjects(projectsUnfiltered, categories) || [];
+
+    const dataConversionTime = timeSpent(start);
+
+    logger.postErrorToLogger(
+        "info",
+        "Timer on index page",
+        `
+        start: ${startTime}\n
+        projects: ${projectsTime}\n
+        blockchainStatisticsTime: ${blockchainStatisticsTime}\n
+        dexesStatistics: ${dexesStatisticsTime}\n
+        tokenStatistics: ${tokenStatisticsTime}\n
+        transactionsData: ${transactionsDataTime}\n
+        data conversion: ${dataConversionTime}
+        `
+    );
     return {
         props: {
             overflow: true,
             headers: req.headers,
-            general_stats:
-                {
-                    blockchain_info: {
-                        ...blockchainStatistics,
-                        low_high_price: tokenStatistics.low_high_price || {},
-                        token_price_chart: tokenStatistics.token_price_chart || [],
-                        trans_history: blockchainStatistics.transactions_history || [],
-                        dex_trading_volume: dexesStatistics.dex_trading_volume || null,
-                        total_value_locked: dexesStatistics.total_value_locked || null,
-                        vol_24h: dexesStatistics.vol_24h || null
-                    },
+            general_stats,
 
-                    dex_tvl: dexesStatistics.dex_tvl_chart || [],
-                    dex_volumes: dexesStatistics.dex_volumes_chart || [],
-                    daily_unique_contract_addresses: dexesStatistics.dex_addresses_chart || [],
-                    daily_contract_transactions: dexesStatistics.dex_transactions_chart || [],
-                    markets: dexesStatistics.dex_markets || [],
-                    token_statistics: tokenStatistics.token_statistics || [],
-                    top_tokens_by_volume: tokenStatistics.top_tokens_by_volume || [],
-
-                    top_statistics: blockchainStatistics.money_flow || {},
-                    transactions_plot: blockchainStatistics.user_transactions_history || [],
-                    addresses_plot: blockchainStatistics.addresses_plot || [],
-                    daily_new_created_wallets: blockchainStatistics.daily_new_created_wallets || [],
-                    active_unique_addresses: blockchainStatistics.active_unique_addresses || {}
-                } || {},
-
-            contract_addresses: dexesStatistics.dex_users_activity?.length
-                ? dexesStatistics.dex_users_activity.map((item) => {
-                      return {
-                          ...item,
-                          "24h": item?.["24h"]?.addresses,
-                          all_time: item?.["all_time"]?.addresses,
-                          week: item?.["week"]?.addresses
-                      };
-                  })
-                : [],
-            contract_transactions: dexesStatistics.dex_users_activity?.length
-                ? dexesStatistics.dex_users_activity.map((item) => {
-                      return {
-                          ...item,
-                          "24h": item?.["24h"]?.transactions,
-                          all_time: item?.["all_time"]?.transactions,
-                          week: item?.["week"]?.transactions
-                      };
-                  })
-                : [],
+            contract_addresses,
+            contract_transactions,
 
             dexes_volumes: dexesStatistics.dexes_volumes || [],
 
-            projects: filtrateProjects(projectsUnfiltered, categories) || [],
-            transactions: (await transactions.getData()) || []
+            projects: projectsData,
+            transactions: transactionsData
         }
     };
 }
