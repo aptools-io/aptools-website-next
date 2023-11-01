2d9d84239565d8a280737b2c8994e61bf1026ddc
Your Name <you@example.com>
2023-11-01 11:47:33 +0300

    fix: hotfix

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -8,22 +8,26 @@ type ResponseData = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-    const dataParsed = JSON.parse(req.body);
-    const { token, password } = dataParsed || {};
-    const promiseData = await auth.registerPassword(token, password, (refreshToken) => {
-        if (refreshToken) setCookie("refreshToken", refreshToken, { req, res, httpOnly: true });
-    });
+    if (req.body) {
+        const dataParsed = JSON.parse(req.body);
+        const { token, password } = dataParsed || {};
+        const promiseData = await auth.registerPassword(token, password, (refreshToken) => {
+            if (refreshToken) setCookie("refreshToken", refreshToken, { req, res, httpOnly: true });
+        });
 
-    const response = promiseData as IUserResponse;
-    if (response.status === "error") {
-        res.status(200).json(response);
+        const response = promiseData as IUserResponse;
+        if (response.status === "error") {
+            res.status(200).json(response);
+        } else {
+            const { response: promisedResponse } = response;
+
+            const { data } = (await promisedResponse) || {};
+            const { accessToken, user } = data || {};
+            if (accessToken) setCookie("accessToken", accessToken, { req, res, httpOnly: true });
+
+            res.status(200).json(await promisedResponse);
+        }
     } else {
-        const { response: promisedResponse } = response;
-
-        const { data } = (await promisedResponse) || {};
-        const { accessToken, user } = data || {};
-        if (accessToken) setCookie("accessToken", accessToken, { req, res, httpOnly: true });
-
-        res.status(200).json(await promisedResponse);
+        res.status(200).json({ status: "error" });
     }
 }
