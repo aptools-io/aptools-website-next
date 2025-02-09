40efef2b97a33abf95b7d09260259040b6466755
31Vector31 <lutaiyura@gmail.com>
2025-02-09 23:27:59 +0200

    CB-19

--- src/scripts/util/strings.ts
+++ src/scripts/util/strings.ts
@@ -1,15 +1,13 @@
-
-const concatString = (string: string | number, left: string = "", right: string = "") => { 
-    if(string === "-" || !string) return "-";
+const concatString = (string: string | number, left: string = "", right: string = "") => {
+    if (string === "-" || !string) return "-";
     return `${left}${string}${right}`;
 };
 
-const shortenHashString = (string: string, divider = [9, 6]) => { 
-    if(string === "-" || !string) return "-";
-    if(string.length < 16 || divider[0] === 0 && divider[1] === 0) return string;
-    
+const shortenHashString = (string: string, divider = [9, 6]) => {
+    if (string === "-" || !string) return "";
+    if (string.length < 16 || (divider[0] === 0 && divider[1] === 0)) return string;
+
     return `${string.slice(0, divider[0])}...${string.slice(-divider[1])}`;
 };
-
 
 export { concatString, shortenHashString };
--- src/scripts/util/transactions.ts
+++ src/scripts/util/transactions.ts
@@ -1,25 +1,30 @@
-
-const getTransactionType = (type: string) => { 
+const getTransactionType = (type: string) => {
     switch (type) {
-    case "user_transaction":
-        return {
-            name: "UserTransaction",
-            color: "blue",
-        };
-    case "state_checkpoint_transaction":
-        return {
-            name: "StateCheckpoint",
-            color: "green",
-        };
-    case "block_metadata_transaction":
-        return {
-            name: "BlockMetadata",
-            color: "red",
-        };
-    default: return {
-        name: "null",
-        color: ""
-    };
+        case "user_transaction":
+            return {
+                name: "UserTransaction",
+                color: "blue"
+            };
+        case "state_checkpoint_transaction":
+            return {
+                name: "StateCheckpoint",
+                color: "green"
+            };
+        case "block_metadata_transaction":
+            return {
+                name: "BlockMetadata",
+                color: "red"
+            };
+        case "block_epilogue_transaction":
+            return {
+                name: "BlockEpilogue",
+                color: "red"
+            };
+        default:
+            return {
+                name: "null",
+                color: ""
+            };
     }
 };
 
